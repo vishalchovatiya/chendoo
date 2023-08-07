13ea892b3e021d2ed02002debf9d4b9a4f29de1e
Vishal Chovatiya <vishalchovatiya@ymail.com>
2023-08-07 04:31:50 +0000

    Enabled debugger

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+## Misc information 
+- Debugging is through vscode metal extension, which uses bloop as build server
+  - Will get `run | debug` option above [Top](chendoo/src/Top.scala) class as shown 
 
+![debug option demo](https://i.imgur.com/5nTnrcS.png)
+
+  - If you don't get the option, try running switching to `bloop` server or run metal doctor to see if debugger is supported or not.
 
 ## References
 - [RISC-V Instruction Set Manual](https://github.com/riscv/riscv-isa-manual/releases/download/riscv-isa-release-1239329-2023-05-23/unpriv-isa-asciidoc.pdf)
--- chendoo/src/Top.scala
+++ chendoo/src/Top.scala
@@ -6,24 +6,24 @@ import mainargs.{Leftover, ParserForMethods, arg, main}
 
 // The Main object extending App to generate the Verilog code.
 object Top {
-    @main
-    def run(
-        // Parse command line arguments and extract required parameters
-        @arg(short = 'c', doc = "Chisel arguments") chiselArgs: Leftover[String],
-      ): Unit = 
-        // Generate Verilog
-        ChiselStage.emitSystemVerilogFile(
-            new GCD(),
-            chiselArgs.value.toArray,
-            Array(
-                "--disable-all-randomization",
-                "--strip-debug-info",
-                "-lower-memories",
-                "--lowering-options=disallowLocalVariables,disallowPackedArrays", // Ref. https://github.com/llvm/circt/issues/4751
-            ),
-        )
+  @main
+  def run(
+      // Parse command line arguments and extract required parameters
+      @arg(short = 'c', doc = "Chisel arguments") chiselArgs: Leftover[String],
+    ): Unit =
+    // Generate Verilog
+    ChiselStage.emitSystemVerilogFile(
+      new GCD(),
+      chiselArgs.value.toArray,
+      Array(
+        "--disable-all-randomization",
+        "--strip-debug-info",
+        "-lower-memories",
+        "--lowering-options=disallowLocalVariables,disallowPackedArrays", // Ref. https://github.com/llvm/circt/issues/4751
+      ),
+    )
 
-    def main(
-        args: Array[String],
-      ): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
+  def main(
+      args: Array[String],
+    ): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
 }
